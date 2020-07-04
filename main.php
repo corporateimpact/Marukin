eb09f296eb7b95049f41b07a6a8db8cac2ce2ef0
horoiwa <horoiwa.yasuhiro@corporate.co.jp>
2020-07-04 11:08:01 +0900

    session start commentout

--- main.php
+++ main.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-if (!isset($_SESSION['USER'])) {
-    header('Location: index.php');
-    exit;
-}
+//if (!isset($_SESSION['USER'])) {
+//    header('Location: index.php');
+//    exit;
+//}
 
 function get_do($str1,$str2){
 	$T = floatval($str1);
