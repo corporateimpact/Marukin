ecc805e5e800811495ae646fe54dde3817ac186d
horoiwa <horoiwa.yasuhiro@corporate.co.jp>
2021-02-28 09:01:25 +0900

    修正３

--- get_amedas_json.php
+++ get_amedas_json.php
@@ -25,7 +25,7 @@ if(0 <= $which && $which <= 3) {
     $json_no = "00";
 } elseif (3 <= $which && $which < 3) {
     $json_no = "03";
-} elseif (6 <= $which && $which < 6) {
+} elseif (6 <= $which && $which < 9) {
     $json_no = "06";
 } elseif (9 <= $which && $which < 12) {
     $json_no = "09";
